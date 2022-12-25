c683e1a919b2b5df5d2d5e05d2fa72db6f9f50d3
Zhenya <ndzhenya@mail.ru>
2022-12-25 16:54:37 +0300

    add Task 10

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a%10;
+if (a > 99)
+{
+    Console.Write("Третье число: ");
+    Console.WriteLine(b);
+}
+if (a < 99)
+{
+        Console.WriteLine("Третьего числа нет ");
+        
+}
